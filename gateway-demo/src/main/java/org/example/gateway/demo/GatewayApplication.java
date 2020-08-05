1f8094cfe6b0e4db7c6d1effd2bea5da34f76649
lvxuhong <812496582@qq.com>
2020-08-05 19:26:01 +0800

    GatewayApplication

---
+++ gateway-demo/src/main/java/org/example/gateway/demo/GatewayApplication.java
@@ -0,0 +1,19 @@
+package org.example.gateway.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author:lvxuhong
+ * @date:2020/8/5
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+}
